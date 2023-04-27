44583ba18bd2bd6cb249d027a92fd9e1c5f200cb
fleuraje <127205495+fleuraje@users.noreply.github.com>
2023-04-27 19:38:12 -0400

    printing the keys from the survey

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ favoriteStore: "ArtStore",
 firstvisit: "false" 
 // breakdown of the values that are listed in the obj called customer , containing all the customer info. 
 };
+
+//some of the information is incorrect like the email , phone ,zipcode and  favorite flavors and they requested this to be changed .
+
+customer["email"] = "finntheHuman@email.com";
+customer["phone"] = "3195551234";
+customer["Zipcode"] = "070707";
+customer["Favoriteflavors"] = "Vanilla, strawberry and oreo";
+
+// they decided to remove ta couple of items from the survey , so I will use the delete keyword to do so .
+
+delete customer.Zipcode 
+delete customer.favoriteStore
+
+
+//now you need to add the some items to the survey 
+
+customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+customer.futureFlavors = "mango";
+customer.todaysPurchaseCost = 5.32;
+
+// and lastly in an array print the keys in your survey 
+console.log(Object.keys(customer));
+
+
+
+
+
